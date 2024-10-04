3b6c7096df2dc98fe2f89ea5b984dfca39eea6fc
JSE955 <jordanscottedwards@gmail.com>
2024-10-04 16:25:12 -0400

    Implement check for diagonal winning sequences

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
 class Game
   def initialize
-    @board = [['X', 'X', 'Y'], ['X', 'Y', 'Y'], ['Y', 8, 9]]
+    @board = [['X', 'X', 'X'], ['X', 'X', 'Y'], ['X', 8, 'Y']]
   end
 
   def check_for_winner(player_mark)
     result = false
-    result = true if check_vertical(player_mark) || check_horizontal(player_mark)
+    result = true if check_vertical(player_mark) || check_horizontal(player_mark) || check_diagonal(player_mark)
     result
   end
 
@@ -24,6 +24,13 @@ class Game
     for col in 0..2
       result = true if [@board[0][col], @board[1][col], @board[2][col]] == [player_mark, player_mark, player_mark]
     end
+    result
+  end
+
+  def check_diagonal(player_mark)
+    player_win = [player_mark, player_mark, player_mark]
+    result = false
+    result = true if [@board[0][0], @board[1][1], @board[2][2]] == player_win || [@board[0][2], @board[1][1], @board[2][0]] == player_win
     result
   end
 end
